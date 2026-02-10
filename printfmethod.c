59aa0e0fcc93e1738b0c292d21ccebc25adcc2fd
HAMZA SALIM MOHAMMAD AMIN ALATHAMNEH <hamzaalathamneh06@gmail.com>
2026-02-10 10:26:48 +0300

    Add files via upload

---
+++ printfmethod.c
@@ -0,0 +1,170 @@
+#include <Windows.h>
+
+#include <stdlib.h>
+
+#include <io.h>
+
+#include <stdarg.h>
+
+#include <stdio.h>
+
+
+
+int print_char(int c) {
+
+    return _write(1, &c, 1);
+
+}
+
+
+
+int print_str(char* str) {
+
+    int count = 0;
+
+    while (*str) {
+
+        print_char((int)*str);
+
+        ++count;
+
+        ++str;
+
+    }
+
+    return count;
+
+}
+
+
+
+int print_digit(long n, int base) {
+
+    int count = 0;
+
+    char* symbols;
+
+
+
+    symbols = "0123456789abcdef";
+
+    if (n < 0) {
+
+        _write(1, "-", 1);
+
+        return print_digit(-n, base) + 1;
+
+    }
+
+    else if (n < base) {
+
+        return print_char(symbols[n]);
+
+    }
+
+    else {
+
+        count = print_digit(n / base, base);
+
+        return count + print_digit(n % base, base);
+
+
+
+    }
+
+
+
+}
+
+
+
+int print_format(char specifier, va_list* ap) {
+
+    int count = 0;
+
+
+
+    if (specifier == 'c') {
+
+        count += print_char(va_arg(*ap, int));
+
+    }
+
+    else if (specifier == 's') {
+
+        count += print_str(va_arg(*ap, char*));
+
+    }
+
+    else if (specifier == 'd') {
+
+        count += print_digit((long)va_arg(*ap, int), 10);
+
+    }
+
+    else if (specifier == 'x') {
+
+        count += print_digit((long)va_arg(*ap, unsigned int), 16);
+
+    }
+
+    else {
+
+        count += _write(1, &specifier, 1);
+
+    }
+
+    return count;
+
+}
+
+
+
+
+
+int my_printf(const char* format, ...) {
+
+    va_list ap;
+
+    int count = 0;
+
+
+
+    va_start(ap, format);
+
+    while (*format != '\0') {
+
+        if (*format == '%') count += print_format(*++format, &ap);
+
+        else count += _write(1, format, 1);
+
+
+
+        ++format;
+
+    }
+
+    va_end(ap);
+
+    return count;
+
+
+
+}
+
+
+
+int main() {
+
+    my_printf("this is my print statement Integer: %d\n  character: %c\n string: %s",1,'C',"This is a string");
+    printf("this is my print statement Integer: %d\n  character: %c\n string: %s", 1, 'C', "This is a string");
+
+
+    return 0;
+
+}
+
+
+
+
+
